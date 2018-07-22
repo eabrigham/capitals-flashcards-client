3ff9cd39a2935e61d11ac6656e436d0febb03475
eabrigham <eabrigham@gmail.com>
2018-07-21 23:37:52 -0400

    Made authentication functional with minor changes.
    Fixed typo addHandlers() so auth events run. Made user messaging
    visible in <p> in html. Console logs.

--- assets/scripts/auth/ui.js
+++ assets/scripts/auth/ui.js
@@ -4,22 +4,24 @@ const store = require('../store.js')
 
 const mismatchedPasswords = function () {
   document.getElementById('sign-up').reset()
-  $('#game-message').text(`Passwords don't match. Please sign up again.`)
+  $('#message').text(`Passwords don't match. Please sign up again.`)
 }
-
 const signUpSuccess = function (data) {
+  console.log('sign up success ran')
   document.getElementById('sign-up').reset()
-  $('#game-message').text('Signed up successfully')
+  $('#message').text('Signed up successfully')
 }
 
 const signUpFailure = function (err) {
+  console.log('sign up failure ran')
   document.getElementById('sign-up').reset()
-  $('#game-message').text('Error on sign up.')
+  $('#message').text('Error on sign up.')
 }
 
 const signInSuccess = function (data) {
+  console.log('sign in success ran')
   document.getElementById('sign-in').reset()
-  $('#game-message').text('Signed in successfully')
+  $('#message').text('Signed in successfully')
   $('#sign-up, #sign-in').css('visibility', 'hidden')
   $('#sign-out, #change-password, #new-game, #display-games').css('visibility', 'visible')
   store.user = data.user
@@ -27,22 +29,22 @@ const signInSuccess = function (data) {
 
 const signInFailure = function (err) {
   document.getElementById('sign-in').reset()
-  $('#game-message').text('Error on sign in. Please check your password.')
+  $('#message').text('Error on sign in. Please check your password.')
 }
 
 const changePasswordSuccess = function (data) {
   document.getElementById('change-password').reset()
-  $('#game-message').text('Changed password successfully')
+  $('#message').text('Changed password successfully')
 }
 
 const changePasswordFailure = function (err) {
   document.getElementById('change-password').reset()
-  $('#game-message').text('Error on change password.')
+  $('#message').text('Error on change password.')
 }
 
 const signOutSuccess = function (data) {
   document.getElementById('sign-out').reset()
-  $('#game-message').text('Signed out successfully')
+  $('#message').text('Signed out successfully')
   $('#sign-up, #sign-in').css('visibility', 'visible')
   $('#sign-out, #change-password, #new-game, #display-games').css('visibility', 'hidden')
   $('#game-board').empty()
@@ -51,7 +53,7 @@ const signOutSuccess = function (data) {
 
 const signOutFailure = function (err) {
   document.getElementById('sign-out').reset()
-  $('#game-message').text('Error on sign out.')
+  $('#message').text('Error on sign out.')
 }
 
 module.exports = {
