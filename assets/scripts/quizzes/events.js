eca79989299c1e58463074db42d98ee97716b6c7
eabrigham <eabrigham@gmail.com>
2018-07-23 15:49:56 -0400

    Added onAnswerQuestion to quizzes/events
    Display user answer w getFormFields.

--- assets/scripts/quizzes/events.js
+++ assets/scripts/quizzes/events.js
@@ -1,3 +1,4 @@
+const getFormFields = require('../../../lib/get-form-fields.js')
 const api = require('./api.js')
 const ui = require('./ui.js')
 
@@ -9,9 +10,21 @@ const onNewQuestion = function(event) {
         .catch((errors) => console.log('get quizzes failed and errors are ', errors))
 }
 
+const onAnswerQuestion = function(event) {
+    event.preventDefault()
+    console.log('In onAnswerQuestion and the event is ', event)
+    const data = getFormFields(event.target)
+    console.log('In onAnswerQuestion and the getFormFields data is ', data)
+    // parse the user's answer using getFormFields
+    // determine whether answer is correct based on the stored value of quiz
+    // ajax request with updated consecutive_correct for quiz
+    // ui to user "correct!" or "the capital is __"
+}
+
 const addHandler = () => {
     console.log('quizzes.addHandler running')
     $('#new-question').on('submit', onNewQuestion)
+    $('#answer').on('submit', onAnswerQuestion)
 }
 
 module.exports = {
--- assets/scripts/quizzes/ui.js
+++ assets/scripts/quizzes/ui.js
@@ -1,11 +1,13 @@
-
+const store = require('./../store.js')
 
 const newQuestion = function (data) {
+    console.log('In ui.newQuestion and the data is ', data)
     console.log('In ui.newQuestion and the quizzes are', data.quizzes)
     const selector = Math.floor(data.quizzes.length * Math.random())
     console.log('selector is ', selector)
     const quiz = data.quizzes[selector]
     console.log('In ui.newQuestion and the quiz is', quiz)
+    store.quiz = quiz
 
     $('#question').text(`What is the capital of ${quiz.card.side_a}`)
 }
